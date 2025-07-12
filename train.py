e1f7b346bc7f3f74724d4ce81df80b0caab1b57e
nguyenvannam2004 <151933018+nguyenvannam2004@users.noreply.github.com>
2025-07-12 11:28:07 +0700

    Update1 train.py

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ X = df.drop("quality", axis=1)
 y = df["quality"]
 
 # Tách train/test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train mô hình
 model = RandomForestRegressor(n_estimators=100, random_state=42)
